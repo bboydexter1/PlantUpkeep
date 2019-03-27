1b0cf83160fc2b9f94f10709146552393205ad46
Sebastian Dyjeta <bboydexter1@gmail.com>
2019-03-27 12:12:06 +0100

    index

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
+#!/usr/bin/env python
 from flask import Flask, render_template
-import RPi.GPIO as GPIO
+#import RPi.GPIO as GPIO
 
-GPIO.setmode(GPIO.BOARD)
-GPIO.setwarnings(False)
-GPIO.setup(8, GPIO.OUT)
-GPIO.output(8, False)
+#GPIO.setmode(GPIO.BOARD)
+#GPIO.setwarnings(False)
+#GPIO.setup(8, GPIO.OUT)
+#GPIO.output(8, False)
 
 app = Flask(__name__)
 
@@ -14,15 +15,15 @@ temp = False
 def index():
     return render_template('index.html')
 
-@app.route('/on')
-def on():
-    GPIO.output(8, True)
-    return render_template('index.html')
+@app.route('/lamp')
+def lamp():
+    #GPIO.output(8, True)
+    return render_template('lamp.html')
 
-@app.route('/off')
-def off():
-    GPIO.output(8, False)
-    return render_template('index.html')
+@app.route('/pump')
+def pump():
+    #GPIO.output(8, False)
+    return render_template('pump.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port= 8080 , debug = True)
