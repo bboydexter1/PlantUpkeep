71c0a8969972df8bb34cae89ed6093f92767bc1a
Sebastian Dyjeta <bboydexter1@gmail.com>
2019-04-15 08:53:08 +0200

    new classes and db

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BOARD)
@@ -9,7 +10,32 @@ GPIO.output(8, False)
 
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
+db = SQLAlchemy(app)
+
 temp = False
+
+class PlantPreset(db.Model):
+    __tablename__ = 'plantPreset'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20), unique=True, nullable=False)
+    lampFrom = db.Column(db.Time, unique=True, nullable=False)
+    lampTo = db.Column(db.Time, unique=True, nullable=False)
+    brightnessID = db.Column(db.Integer, db.ForeignKey('brightness.id'), nullable=True)
+    humidityID = db.Column(db.Integer, db.ForeignKey('humidity.id'), nullable=True)
+
+class Brightness():
+    __tablename__ = 'brightness'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20), unique=True, nullable=False)
+    brightness = db.Column(db.Integer, nullable=False)
+    
+class Humidity():
+    __tablename__ = 'humidity'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20), unique=True, nullable=False)
+    soilHumidity = db.Column(db.Integer, nullable=False)
 
 @app.route('/')
 def index():
