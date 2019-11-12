bb6bd5c429fb8fa0fd3df9531ca8f037cf632080
Sebastian Dyjeta <bboydexter1@gmail.com>
2019-11-12 19:53:32 +0100

    more thesis text

--- examples.py
+++ examples.py
@@ -1,14 +1,4 @@
-#!/usr/bin/env python
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port = 80 , debug = True)
-
-
+while True:
+    schedule.run_pending()
+    time.sleep(1)
 
