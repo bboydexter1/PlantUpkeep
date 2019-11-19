6767d3b3583d23286eefd9edee2fdd1f69d29bb0
Sebastian Dyjeta <bboydexter1@gmail.com>
2019-11-19 15:29:50 +0100

    added info about views to thesis and did some clean ups befor prezentation

--- examples.py
+++ examples.py
@@ -1,10 +1,4 @@
-[Unit]
-Description=plant upkeep system service
-After=multi-user.target
-[Service]
-Type=idle
-ExecStart=/usr/bin/python /home/pi/Farma/main.py
-[Install]
-WantedBy=multi-user.target
+#!/usr/bin/env python
+import GPIOFuntions
 
-mv /lib/systemd/system/plantUpkeep  /lib/systemd/system/plant.upkeep
+GPIOFuntions.test()
